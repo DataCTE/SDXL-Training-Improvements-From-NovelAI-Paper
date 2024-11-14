4112a295e1d112a785c77206775f5eb091f65794
DataVoid <izquierdoxander@gmail.com>
2024-11-14 08:31:01 -0500

    __init__

---
+++ src/__init__.py
@@ -0,0 +1,87 @@
+from training import (
+    EMAModel,
+    training_loss_v_prediction,
+    get_sigmas,
+    PerceptualLoss,
+    get_resolution_dependent_sigma_max,
+    VAEFineTuner
+)
+
+from data import (
+    CustomDataset,
+    custom_collate,
+    TagBasedLossWeighter,
+    UltimateUpscaler,
+    USDUMode,
+    USDUSFMode
+)
+
+from utils import (
+    save_checkpoint,
+    load_checkpoint,
+    save_final_outputs,
+    cleanup,
+    get_device,
+    to_device,
+    push_to_hub,
+    setup_training,
+    verify_training_components,
+    setup_logging,
+    log_system_info,
+    log_training_setup,
+    log_gpu_memory,
+    setup_wandb,
+    cleanup_wandb,
+    create_model_card,
+    save_model_card,
+    setup_models,
+    verify_models,
+    validate_dataset
+)
+
+from inference import SDXLInference
+
+__version__ = "0.1.0"
+
+__all__ = [
+    # Training
+    'EMAModel',
+    'training_loss_v_prediction',
+    'get_sigmas',
+    'PerceptualLoss',
+    'get_resolution_dependent_sigma_max',
+    'VAEFineTuner',
+    
+    # Data
+    'CustomDataset',
+    'custom_collate',
+    'TagBasedLossWeighter',
+    'UltimateUpscaler',
+    'USDUMode',
+    'USDUSFMode',
+    
+    # Utils
+    'save_checkpoint',
+    'load_checkpoint',
+    'save_final_outputs',
+    'cleanup',
+    'get_device',
+    'to_device',
+    'push_to_hub',
+    'setup_training',
+    'verify_training_components',
+    'setup_logging',
+    'log_system_info',
+    'log_training_setup',
+    'log_gpu_memory',
+    'setup_wandb',
+    'cleanup_wandb',
+    'create_model_card',
+    'save_model_card',
+    'setup_models',
+    'verify_models',
+    'validate_dataset',
+    
+    # Inference
+    'SDXLInference'
+]
